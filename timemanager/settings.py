b47a3b49727ca4747e301d2fcad4927916e9c07b
Frozet <32857464+Frozet@users.noreply.github.com>
2024-01-20 19:59:32 +0800

    Update settings.py

--- timemanager/settings.py
+++ timemanager/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@d5ma0a5#rrj0-3(qj!_=9%2mppdgbptky5(i+*(!@au)pmo@+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', '192.168.0.17']
 
 
 # Application definition

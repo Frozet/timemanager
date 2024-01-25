fe68ccce80a94895ba2ccfac78254cd482c7872d
Frozet <32857464+Frozet@users.noreply.github.com>
2024-01-25 16:50:09 +0800

    lesson 11 and 12

--- task/views.py
+++ task/views.py
@@ -3,14 +3,26 @@ from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.template.loader import render_to_string
 
+menu = ["О сайте", "Добавить статью", "Обратная связь", "Войти"]
+
+
+data_db = [
+    {'id': 1, 'title': 't1', 'content': 'text1', 'is_published': True},
+    {'id': 2, 'title': 't2', 'content': 'text2', 'is_published': False},
+    {'id': 3, 'title': 't3', 'content': 'text3', 'is_published': True},
+]
+
 # Create your views here.
 def index(request):
-    # t = render_to_string('timemanager/index.html')
-    # return HttpResponse(t)
-    return render(request, 'timemanager/index.html')
+    data = {
+        'title': 'главная страница',
+        'menu': menu,
+        'posts': data_db,
+    }
+    return render(request, 'timemanager/index.html', context=data)
 
 def about(request):
-    return render(request, 'timemanager/about.html')
+    return render(request, 'timemanager/about.html', {'title': 'О сайте'})
 
 def cats(request, cat_id):
     return HttpResponse(f'Страница с категориями<p>id: {cat_id}</p>')
